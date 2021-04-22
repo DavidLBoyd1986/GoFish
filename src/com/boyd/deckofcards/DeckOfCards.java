5f75eebefc2e96d703b5b041f730332598f14e4b
David <davidlboyd1986@gmail.com>
2021-04-22 14:22:46 -0400

    Worked on DeckOfCards. Might make Deck a Set instead of Array List

--- src/com/boyd/deckofcards/DeckOfCards.java
+++ src/com/boyd/deckofcards/DeckOfCards.java
@@ -1,19 +1,21 @@
 package com.boyd.deckofcards;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Random;
 
 class DeckOfCards implements DeckOfCardsInterface {
 
-	private final int numOfCards = 52;
+	private final int totalNumOfCards = 52;
 	public enum Suit {DIAMOND, HEART, CLUB, SPADE};
 	public enum Rank {TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING, ACE};
 	public ArrayList<Card> deck = new ArrayList<Card>();
 	
 	public DeckOfCards() {
 
-		for (Suit suit : Suit.values()) {
-			for (Rank rank : Rank.values() ) {
+		for ( Suit suit : Suit.values() ) {
+			for ( Rank rank : Rank.values() ) {
 				Card card = new Card(suit, rank);
 				deck.add(card);
 			}
@@ -30,34 +32,97 @@ class DeckOfCards implements DeckOfCardsInterface {
 	public int getNumCards() {
 		return deck.size();
 	}
+
+
+	@Override
+	public void shuffleDeck() {
+		Collections.shuffle(deck);
+	}
 	
 	@Override
 	public Card getTopCard() {
-		return deck.get(0);
+		Card card = deck.get(0);
+		deck.remove(0);
+		return card;
 	}
 
 	@Override
 	public Card getBottomCard() {
-		return deck.get(deck.size()-1);
+		Card card = deck.get(deck.size() - 1);
+		deck.remove(deck.size() - 1);
+		return card;
 	}
 	
 	@Override
 	public Card getSpecificCard(int index) {
-		return deck.get(index);
+		Card card = deck.get(index);
+		deck.remove(index);
+		return card;
 	}
 	
 	@Override
 	public Card getRandomCard() {
 		Random random = new Random();
 		int randomIndex = random.nextInt(deck.size());
-		return deck.get(randomIndex);
+		Card card = deck.get(randomIndex);
+		deck.remove(randomIndex);
+		return card;
 	}
 	
 	@Override
-	public Card shuffleDeck() {
-		// TODO Auto-generated method stub
+	public Card[] getCardsTop(int numOfCards) {
+		Card[] cards = new Card[numOfCards];
+		int count = 0;
+		while ( count < numOfCards ) {
+			cards[count] = deck.get(count);
+			deck.remove(count);
+			count++;
+		}
+		return cards;
 	}
 	
+	@Override
+	public Card[] getCardsBottom(int numOfCards) {
+		Card[] cards = new Card[numOfCards];
+		int count = 0;
+		while ( count < numOfCards ) {
+			cards[count] = deck.get(deck.size() - 1);
+			deck.remove(deck.size() - 1);
+			count ++;
+		}
+		return cards;
+	}
+	
+	@Override
+	public Card[] getCardsSpecific(int[] cardsSelected) {
+		Card[] cards = new Card[cardsSelected.length];
+		// Array must be sorted so it always gets the earliest card in the array first
+		Arrays.sort(cardsSelected);
+		int count = 0;
+		for ( int i : cardsSelected) {
+			// Since the array gets smaller I have to subtract the # of items removed to get the selection specified
+			Card card = deck.get(i - count);
+			deck.remove(i - count);
+			cards[count] = card;
+			count ++;
+		}
+		return cards;
+	}
+	
+	@Override
+	public Card[] getCardsRandom(int numOfCards) {
+		Random random = new Random();
+		Card[] cards = new Card[numOfCards];
+		int count = 0;
+		while ( count < numOfCards ) {
+			int randomInt = random.nextInt(deck.size());
+			cards[count] = deck.get(randomInt);
+			deck.remove(randomInt);
+			count ++;
+		}
+		return cards;
+	}
+
 	public String toString() {
 		return deck.toString();
 	}
@@ -73,7 +138,35 @@ class DeckOfCards implements DeckOfCardsInterface {
 		System.out.println("SpecificCard = " + specificCard);
 		Card randomCard = deck.getRandomCard();
 		System.out.println("RandCard = " + randomCard);
-
+		deck.shuffleDeck();
+		System.out.println(deck);
+		Card[] cardsTop = deck.getCardsTop(4);
+		System.out.println(cardsTop);
+		for ( Card card : cardsTop) {
+			System.out.println(card);
+		}
+		System.out.println(deck);
+		Card[] cardsBottom = deck.getCardsBottom(5);
+		System.out.println(cardsBottom);
+		for ( Card card :cardsBottom) {
+			System.out.println(card);
+		}
+		System.out.println(deck.getNumCards());
+		Card[] cardsRandom = deck.getCardsRandom(3);
+		System.out.println(cardsRandom);
+		for ( Card card : cardsRandom ) {
+			System.out.println(card);
+		}
+		System.out.println(deck.getNumCards());
+		System.out.println(deck);
+		int[] cardsToGet = {1, 3, 4};
+		Card[] cardsSpecific = deck.getCardsSpecific(cardsToGet);
+		System.out.println(cardsSpecific);
+		for ( Card card : cardsSpecific) {
+			System.out.println(card);
+		}
+		System.out.println(deck);
+		
 	}
 
 	
--- src/com/boyd/deckofcards/DeckOfCardsInterface.java
+++ src/com/boyd/deckofcards/DeckOfCardsInterface.java
@@ -19,7 +19,27 @@ public interface DeckOfCardsInterface {
 	
 	public Card getRandomCard();
 	
-	public Card shuffleDeck();
+	public Card[] getCardsTop(int numOfCards);
+	
+	public Card[] getCardsBottom(int numOfCards);
+	
+	public Card[] getCardsSpecific(int[] cardsSelected);
+	
+	public Card[] getCardsRandom(int numOfCards);
+	
+	public void addCardTop(Card card);
+	
+	public void addCardBottom(Card card);
+	
+	public void addCardRandom(Card card);
+	
+	public void addCardsTop(Card[] cards);
+	
+	public void addCardsBottom(Card[] cards);
+	
+	public void addCardsRandom(Card[] cards);
+	
+	public void shuffleDeck();
 	
 	public void dealCards(int numOfPlayers, int numOfCards);
 	
