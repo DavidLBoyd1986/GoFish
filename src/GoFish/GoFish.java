63ccd498a1d7461967abcd4fada928ada2e7f9ee
David <davidlboyd1986@gmail.com>
2021-05-05 15:29:42 -0400

    Starting development of actual GoFish game

---
+++ src/GoFish/GoFish.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package GoFish;
+
+/**
+ * @author David
+ *
+ */
+public class GoFish implements GoFishInterface {
+
+	/**
+	 * 
+	 */
+	public GoFish() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
